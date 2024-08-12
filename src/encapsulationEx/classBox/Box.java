63d0aece950687efdf87c2f5a2fe83285a0d07a8
Nikolay Grudov <nickolayg@gmail.com>
2024-08-12 16:27:37 +0300

    update

---
+++ src/encapsulationEx/classBox/Box.java
@@ -0,0 +1,28 @@
+package encapsulationEx.classBox;
+
+public class Box {
+    private double length;
+    private double width;
+    private double height;
+
+    public Box(double length, double width, double height) {
+        this.setLength(length);
+        this.setWidth(width);
+        this.setHeight(height);
+    }
+
+    public void setLength(double length) {
+        if (length <= 0) {
+            throw new IllegalArgumentException("Width cannot be zero or negative.");
+        }
+        this.length = length;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+}
---
+++ src/encapsulationEx/classBox/Main.java
@@ -0,0 +1,8 @@
+package encapsulationEx.classBox;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        }
+    }
